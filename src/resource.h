772422368fdd49668e29021a4e93f5833a414c53
EugeneManushkin <eugene.manushkin@gmail.com>
2018-06-14 18:39:02 +0300

    Increment build number

--- src/resource.h
+++ src/resource.h
@@ -14,7 +14,7 @@
 #endif
 #define CTAGS_VERSION_MAJOR 2
 #define CTAGS_VERSION_MINOR 1
-#define CTAGS_BUILD 18
+#define CTAGS_BUILD 19
 #define CTAGS_FILENAME "ctags.dll"
 #define CTAGS_PRODUCT_NAME L"Ctags Source Navigator"
 #define CTAGS_FILE_DESCR L"Far Manager source code browsing plugin"
