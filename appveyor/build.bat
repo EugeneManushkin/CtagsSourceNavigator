0ba6f42d9849e40f95da978af14e029922315c07
eugenemanushkin <eugene.manushkin@gmail.com>
2018-11-16 23:52:40 +0300

    Extract test data

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -5,6 +5,5 @@ cmake -DCTAGS_VERSION_MAJOR=%CTAGS_VERSION_MAJOR% -DCTAGS_VERSION_MINOR=%CTAGS_V
 msbuild /target:build /property:Configuration=%CONFIGURATION% /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" %BUILD_ROOT%\Project.sln || exit /b 1
 
 cd %BUILD_ROOT%\%CONFIGURATION%\ctags || exit /b 1
-set BUILD_NUM=%CTAGS_VERSION_MAJOR%.%CTAGS_VERSION_MINOR%.0.%CTAGS_BUILD%
-7z a ctags-%BUILD_NUM%-%ARCH%.zip * || exit /b 1
-move ctags-%BUILD_NUM%-%ARCH%.zip "%APPVEYOR_BUILD_FOLDER%" || exit /b 1
+7z a ctags-%CTAGS_VERSION%-%ARCH%.zip * || exit /b 1
+move ctags-%CTAGS_VERSION%-%ARCH%.zip "%APPVEYOR_BUILD_FOLDER%" || exit /b 1
--- appveyor/test.bat
+++ appveyor/test.bat
@@ -1,4 +1,5 @@
 echo on
 cd "%BUILD_ROOT%"\tests\%CONFIGURATION%
+for %%G in ("%APPVEYOR_BUILD_FOLDER%"\tests\tags\*.zip) do ( cmake -E tar xzvf "%%G" || exit /b 1 )
 tags_tests.exe
 tags_tests.exe --CheckIdxFiles
