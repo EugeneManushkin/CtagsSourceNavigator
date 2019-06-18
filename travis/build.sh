bd45217a0c25827389de944eda150303f7413d73
EugeneManushkin <Eugene.Manushkin@gmail.com>
2019-06-18 20:16:41 +0300

    Run tests in travis/test.sh

--- travis/build.sh
+++ travis/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 mkdir $BUILD_DIR && cd $BUILD_DIR
 cmake -DCMAKE_CXX_STANDARD=11 $TRAVIS_BUILD_DIR/tests
 make
--- travis/test.sh
+++ travis/test.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-echo TODO: run tests
+#!/bin/bash -x
+cd $TRAVIS_BUILD_DIR/$BUILD_DIR
+
+for arch in $TRAVIS_BUILD_DIR/tests/tags/*.zip; do
+  unzip "$arch"
+done
+
+./tags_tests
+./tags_tests --CheckIdxFiles
+./tag_view_tests
+./tags_cache_tests
