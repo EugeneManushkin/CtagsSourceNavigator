fc4cc3ea76bf3cd110fc6989850f0358fef8d1fe
EugeneManushkin <eugene.manushkin@gmail.com>
2020-09-29 00:24:40 +0300

    Simplified TagsCache tests

--- tests/tags_cache/main.cpp
+++ tests/tags_cache/main.cpp
@@ -1,6 +1,21 @@
 #include <gtest/gtest.h>
 #include <tags_cache.h>
 
+bool operator == (TagInfo const& left, TagInfo const& right)
+{
+  return left.name == right.name
+      && left.file == right.file
+      && left.re == right.re
+      && left.lineno == right.lineno
+      && left.type == right.type
+      && left.info == right.info;
+}
+
+std::ostream& operator<<(std::ostream& os, TagInfo const& tag)
+{
+  return os << tag.name;
+}
+
 namespace Tags
 {
 namespace Internal
@@ -24,15 +39,8 @@ namespace Internal
     auto const ThirdTag = MakeTag("Name3");
     auto const FourthTag = MakeTag("Name4");
 
-    bool Equal(TagInfo const& left, TagInfo const& right)
-    {
-      return left.name == right.name
-          && left.file == right.file
-          && left.re == right.re
-          && left.lineno == right.lineno
-          && left.type == right.type
-          && left.info == right.info;
-    }
+    using TAGS=std::vector<TagInfo>;
+    using STAT=std::vector<std::pair<TagInfo, size_t>>;
 
     std::shared_ptr<TagsCache> MakeCache(std::vector<TagInfo>&& tags, size_t capacity = 0)
     {
@@ -43,84 +51,50 @@ namespace Internal
       return cache;
     }
 
-    TagInfo TagAt(size_t index, std::shared_ptr<TagsCache> const& cache)
-    {
-      return cache->Get().at(index);
-    }
-
-    size_t FreqAt(size_t index, std::shared_ptr<TagsCache> const& cache)
-    {
-      return cache->GetStat().at(index).second;
-    }
-
-    size_t Size(std::shared_ptr<TagsCache> const& cache)
-    {
-      return cache->Get().size();
-    }
-
     TEST(TagsCache, InsertsTag)
     {
       auto sut = MakeCache({FirstTag});
-      ASSERT_EQ(1, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_EQ(1, FreqAt(0, sut));
+      ASSERT_EQ(TAGS({FirstTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, LastAddedTagsGoFirst)
     {
-      size_t const totalTags = 3;
       auto sut = MakeCache({FirstTag, ThirdTag, SecondTag});
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(1, sut)));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(2, sut)));
-      ASSERT_EQ(1, FreqAt(0, sut));
-      ASSERT_EQ(1, FreqAt(1, sut));
-      ASSERT_EQ(1, FreqAt(2, sut));
+      ASSERT_EQ(TAGS({SecondTag, ThirdTag, FirstTag}), sut->Get());
+      ASSERT_EQ(STAT({{SecondTag, 1}, {ThirdTag, 1}, {FirstTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, IncrementsFrequencyOfSameTags)
     {
-      size_t const totalTags = 1;
       size_t const numberOfInsertions = 10;
-      auto sut = MakeCache(std::vector<TagInfo>(numberOfInsertions, FirstTag), totalTags);
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_EQ(numberOfInsertions, FreqAt(0, sut));
+      auto sut = MakeCache(std::vector<TagInfo>(numberOfInsertions, FirstTag));
+      ASSERT_EQ(TAGS({FirstTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, numberOfInsertions}}), sut->GetStat());
     }
 
     TEST(TagsCache, IncrementsFrequencyOfSameTagsOnSpecifiedFrequency)
     {
-      size_t const totalTags = 1;
       size_t const numberOfInsertions = 10;
       size_t const frequency = 5;
-      auto sut = MakeCache(std::vector<TagInfo>(numberOfInsertions, FirstTag), 1);
+      auto sut = MakeCache(std::vector<TagInfo>(numberOfInsertions, FirstTag));
       sut->Insert(FirstTag, frequency);
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_EQ(numberOfInsertions + frequency, FreqAt(0, sut));
+      ASSERT_EQ(TAGS({FirstTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, numberOfInsertions + frequency}}), sut->GetStat());
     }
 
     TEST(TagsCache, MostFrequentTagsGoFirst)
     {
-      size_t const totalTags = 3;
       auto sut = MakeCache({ThirdTag, SecondTag, ThirdTag, FirstTag, ThirdTag, SecondTag});
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(1, sut)));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(2, sut)));
-      ASSERT_EQ(3, FreqAt(0, sut));
-      ASSERT_EQ(2, FreqAt(1, sut));
-      ASSERT_EQ(1, FreqAt(2, sut));
+      ASSERT_EQ(TAGS({ThirdTag, SecondTag, FirstTag}), sut->Get());
+      ASSERT_EQ(STAT({{ThirdTag, 3}, {SecondTag, 2}, {FirstTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, NewTagRemovesLeastFrequentTag)
     {
-      size_t const totalTags = 2;
-      auto sut = MakeCache({FirstTag, ThirdTag, FirstTag, SecondTag}, totalTags);
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(1, sut)));
-      ASSERT_EQ(2, FreqAt(0, sut));
-      ASSERT_EQ(1, FreqAt(1, sut));
+      auto sut = MakeCache({FirstTag, ThirdTag, FirstTag, SecondTag}, 2);
+      ASSERT_EQ(TAGS({FirstTag, SecondTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, 2}, {SecondTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, ChangingCapacityNotRemovesTags)
@@ -129,47 +103,34 @@ namespace Internal
       size_t const smallerCapacity = 1;
       size_t const largerCapacity = totalTags * 2;
       auto sut = MakeCache({FirstTag, SecondTag, ThirdTag});
-      ASSERT_EQ(totalTags, Size(sut));
+      ASSERT_EQ(TAGS({ThirdTag, SecondTag, FirstTag}), sut->Get());
       sut->SetCapacity(smallerCapacity);
-      ASSERT_EQ(smallerCapacity, Size(sut));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(0, sut)));
+      ASSERT_EQ(TAGS({ThirdTag}), sut->Get());
       sut->SetCapacity(largerCapacity);
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(1, sut)));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(2, sut)));
+      ASSERT_EQ(TAGS({ThirdTag, SecondTag, FirstTag}), sut->Get());
     }
 
     TEST(TagsCache, ReturnsStatisticsForTagsOutOfCapacity)
     {
-      size_t const totalTags = 4;
       size_t const smallerCapacity = 2;
       auto sut = MakeCache({FourthTag, ThirdTag, SecondTag, FirstTag});
       sut->SetCapacity(smallerCapacity);
-      ASSERT_EQ(smallerCapacity, Size(sut));
-      auto const stat = sut->GetStat();
-      ASSERT_EQ(totalTags, stat.size());
-      ASSERT_TRUE(Equal(TagAt(0, sut), stat[0].first));
-      ASSERT_TRUE(Equal(TagAt(1, sut), stat[1].first));
-      ASSERT_TRUE(Equal(ThirdTag, stat[2].first));
-      ASSERT_TRUE(Equal(FourthTag, stat[3].first));
+      ASSERT_EQ(TAGS({FirstTag, SecondTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, 1}, {SecondTag, 1}, {ThirdTag, 1}, {FourthTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, ReturnsSpecifiedNumberOfTags)
     {
-      size_t const totalTags = 4;
-      size_t const limit = totalTags / 2;
       auto sut = MakeCache({FirstTag, SecondTag, ThirdTag, FourthTag});
-      ASSERT_EQ(limit, sut->Get(limit).size());
+      ASSERT_EQ(TAGS({FourthTag, ThirdTag}), sut->Get(2));
     }
 
     TEST(TagsCache, ReturnsTagsLimitedByCapacity)
     {
-      size_t const totalTags = 4;
-      size_t const smallerCapacity = totalTags / 2;
+      size_t const smallerCapacity = 2;
       auto sut = MakeCache({FirstTag, SecondTag, ThirdTag, FourthTag});
       sut->SetCapacity(smallerCapacity);
-      ASSERT_EQ(smallerCapacity, sut->Get(totalTags).size());
+      ASSERT_EQ(TAGS({FourthTag, ThirdTag}), sut->Get(smallerCapacity * 2));
     }
 
     TEST(TagsCache, ShrinkingToFitAndTagModificationNotRemovesTags)
@@ -179,10 +140,7 @@ namespace Internal
       auto sut = MakeCache({FirstTag, FirstTag, FirstTag, FirstTag, SecondTag, SecondTag, ThirdTag}, largerCapacity);
       sut->SetCapacity(totalTags);
       sut->Insert(SecondTag);
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(1, sut)));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(2, sut)));
+      ASSERT_EQ(TAGS({FirstTag, SecondTag, ThirdTag}), sut->Get());
     }
 
     TEST(TagsCache, ModificationTagOutOfCapacityRemovesLastTagInCapacity)
@@ -191,13 +149,10 @@ namespace Internal
       size_t const largerCapacity = totalTags * 2;
       size_t const smallerCapacity = totalTags - 1;
       auto sut = MakeCache({FirstTag, FirstTag, FirstTag, FirstTag, SecondTag, SecondTag, SecondTag, ThirdTag}, largerCapacity);
-      ASSERT_EQ(3, FreqAt(1, sut));
+      ASSERT_EQ(STAT({{FirstTag, 4}, {SecondTag, 3}, {ThirdTag, 1}}), sut->GetStat());
       sut->SetCapacity(smallerCapacity);
       sut->Insert(ThirdTag);
-      ASSERT_EQ(smallerCapacity, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(1, sut)));
-      ASSERT_EQ(2, FreqAt(1, sut));
+      ASSERT_EQ(STAT({{FirstTag, 4}, {ThirdTag, 2}}), sut->GetStat());
     }
 
     TEST(TagsCache, InsertingNewTagRemovesAllTagsOutOfCapacity)
@@ -208,14 +163,11 @@ namespace Internal
       auto sut = MakeCache({FirstTag, FirstTag, FirstTag, FirstTag, SecondTag, SecondTag, SecondTag, ThirdTag, ThirdTag}, largerCapacity);
       sut->SetCapacity(smallerCapacity);
       sut->Insert(FourthTag);
-      ASSERT_EQ(smallerCapacity, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(FourthTag, TagAt(1, sut)));
+      ASSERT_EQ(TAGS({FirstTag, FourthTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, 4}, {FourthTag, 1}}), sut->GetStat());
       sut->Insert(SecondTag);
-      ASSERT_EQ(smallerCapacity, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(1, sut)));
-      ASSERT_EQ(1, FreqAt(1, sut));
+      ASSERT_EQ(TAGS({FirstTag, SecondTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, 4}, {SecondTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, ReturnsTagsInReversedOrderTagsShouldBeInserted)
@@ -223,9 +175,7 @@ namespace Internal
       auto cache = MakeCache({FirstTag, SecondTag, ThirdTag});
       auto tags = cache->Get();
       auto sut = MakeCache(std::vector<TagInfo>(tags.rbegin(), tags.rend()));
-      ASSERT_TRUE(Equal(TagAt(0, cache), TagAt(0, sut)));
-      ASSERT_TRUE(Equal(TagAt(1, cache), TagAt(1, sut)));
-      ASSERT_TRUE(Equal(TagAt(2, cache), TagAt(2, sut)));
+      ASSERT_EQ(cache->Get(), sut->Get());
     }
 
     TEST(TagsCache, ReturnsTagsStatisticsInReversedOrderTagsShouldBeInserted)
@@ -236,49 +186,38 @@ namespace Internal
       for (auto i = stat.rbegin(); i != stat.rend(); ++i)
         sut->Insert(i->first, i->second);
       
-      ASSERT_TRUE(Equal(TagAt(0, cache), TagAt(0, sut)));
-      ASSERT_TRUE(Equal(TagAt(1, cache), TagAt(1, sut)));
-      ASSERT_TRUE(Equal(TagAt(2, cache), TagAt(2, sut)));
-      ASSERT_EQ(FreqAt(0, cache), FreqAt(0, sut));
-      ASSERT_EQ(FreqAt(1, cache), FreqAt(1, sut));
-      ASSERT_EQ(FreqAt(2, cache), FreqAt(2, sut));
+      ASSERT_EQ(cache->GetStat(), sut->GetStat());
     }
 
     TEST(TagsCache, ErasesTag)
     {
       auto sut = MakeCache({FirstTag});
       ASSERT_NO_THROW(sut->Erase(FirstTag));
-      ASSERT_EQ(0, Size(sut));
-      ASSERT_EQ(0, sut->GetStat().size());
+      ASSERT_EQ(TAGS(), sut->Get());
+      ASSERT_EQ(STAT(), sut->GetStat());
     }
 
     TEST(TagsCache, ErasesMiddleTag)
     {
       auto sut = MakeCache({FirstTag, FirstTag, FirstTag, SecondTag, SecondTag, ThirdTag});
-      auto const totalTags = Size(sut);
       ASSERT_NO_THROW(sut->Erase(SecondTag));
-      ASSERT_EQ(totalTags - 1, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(ThirdTag, TagAt(1, sut)));
-      ASSERT_EQ(3, FreqAt(0, sut));
-      ASSERT_EQ(1, FreqAt(1, sut));
+      ASSERT_EQ(STAT({{FirstTag, 3}, {ThirdTag, 1}}), sut->GetStat());
     }
 
     TEST(TagsCache, NotChangesEmptyCacheWhileErasingTag)
     {
       auto sut = CreateTagsCache(0);
       ASSERT_NO_THROW(sut->Erase(FirstTag));
-      ASSERT_EQ(0, Size(sut));
+      ASSERT_EQ(TAGS(), sut->Get());
+      ASSERT_EQ(STAT(), sut->GetStat());
     }
 
     TEST(TagsCache, NotChangesCacheWhileErasingNotFoundTag)
     {
       auto sut = MakeCache({SecondTag, FirstTag});
-      auto const totalTags = Size(sut);
       ASSERT_NO_THROW(sut->Erase(ThirdTag));
-      ASSERT_EQ(totalTags, Size(sut));
-      ASSERT_TRUE(Equal(FirstTag, TagAt(0, sut)));
-      ASSERT_TRUE(Equal(SecondTag, TagAt(1, sut)));
+      ASSERT_EQ(TAGS({FirstTag, SecondTag}), sut->Get());
+      ASSERT_EQ(STAT({{FirstTag, 1}, {SecondTag, 1}}), sut->GetStat());
     }
   }
 }
