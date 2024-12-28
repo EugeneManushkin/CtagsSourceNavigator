2c6b7630eadc299a9eb8df8b8c84b26a25cd6ed5
EugeneManushkin <eugene.manushkin@gmail.com>
2024-12-28 15:03:39 +0300

    Adapt run.bat script for Visual Studio 2019

--- tests/run.bat
+++ tests/run.bat
@@ -6,12 +6,12 @@ set GENERATOR=%~1
 set REPO_ROOT=%~dp0\..
 set ROOT=%~dp0.temp
 
-if "%GENERATOR%"=="" set GENERATOR=Visual Studio 14 2015
+if "%GENERATOR%"=="" set GENERATOR=Visual Studio 16 2019
 
 %~d0
 call :CleanRoot
-call :BuildPlatform x86 "%GENERATOR%"
-call :BuildPlatform x64 "%GENERATOR% Win64"
+call :BuildPlatform x86 "%GENERATOR%" "-A Win32"
+call :BuildPlatform x64 "%GENERATOR%" "-A x64"
 exit /b
 
 :CleanRoot
@@ -22,7 +22,7 @@ exit /b
 :BuildPlatform
   set BUILD_ROOT=%ROOT%\bin\%1
   md "%BUILD_ROOT%" && cd "%BUILD_ROOT%" || exit /b 1
-  cmake -G "%~2" "%REPO_ROOT%" || exit /b 1
+  cmake -G "%~2" "%~3" "%REPO_ROOT%" || exit /b 1
   cmake --build ./ --config Release -j 6 || exit /b 1
   call :RunTests %1 "%BUILD_ROOT%"\tests\Release
 exit /b
