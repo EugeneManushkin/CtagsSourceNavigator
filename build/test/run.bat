6d10fae4f8647b0bb52658cf36453434991ab6b1
EugeneManushkin <eugene.manushkin@gmail.com>
2025-03-07 18:17:15 +0300

    Separate build scripts from appveyor

--- build/test/run.bat
+++ build/test/run.bat
@@ -1,9 +1,11 @@
 echo on
-cd "%BUILD_ROOT%"\tests\%CONFIGURATION%
-for %%G in ("%APPVEYOR_BUILD_FOLDER%"\tests\tags\*.zip) do ( cmake -E tar xzvf "%%G" || exit /b 1 )
+cd "%BUILD_ROOT%"\tests\%CMAKE_CONFIGURATION%
+for %%G in ("%~dp0..\.."\tests\tags\*.zip) do ( cmake -E tar xzvf "%%G" || exit /b 1 )
 tags_tests.exe || exit /b 1
 tags_tests.exe --CheckIdxFiles || exit /b 1
 tags_cache_tests.exe || exit /b 1
 tag_view_tests.exe || exit /b 1
 tags_repository_storage_tests.exe || exit /b 1
 safe_call_tests.exe || exit /b 1
+
+@echo Done.
