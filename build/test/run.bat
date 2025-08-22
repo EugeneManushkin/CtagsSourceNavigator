9a74144ef4a6713fc683a096358f6f4a200f9bf2
EugeneManushkin <eugene.manushkin@gmail.com>
2025-08-22 16:40:31 +0300

    Reorganize tests

--- build/test/run.bat
+++ build/test/run.bat
@@ -1,16 +1,20 @@
 echo on
+set EXPECTED_TESTS=9
+set ACTUAL_TESTS=0
 cd "%BUILD_ROOT%"\tests\%CMAKE_CONFIGURATION%
 for %%G in ("%~dp0..\.."\tests\tags\*.zip) do ( cmake -E tar xzvf "%%G" || exit /b 1 )
-tags_tests.exe || exit /b 1
-tags_tests.exe --CheckIdxFiles || exit /b 1
-tags_cache_tests.exe || exit /b 1
-tags_name_path_line_tests.exe || exit /b 1
-tag_view_tests.exe || exit /b 1
-tags_repository_storage_tests.exe || exit /b 1
-safe_call_tests.exe || exit /b 1
 
-cd "%BUILD_ROOT%"\tests\facade\far3\%CMAKE_CONFIGURATION%
-break_keys.exe || exit /b 1
-guid.exe || exit /b 1
+cd "%BUILD_ROOT%"\tests
+for /R %%G in (*_tests.exe) do call :RunTest "%%~G" || exit /b 1
+cd "%BUILD_ROOT%"\tests
+for /R %%G in (*tags_tests.exe) do call :RunTest "%%~G" --CheckIdxFiles || exit /b 1
+if %ACTUAL_TESTS% LSS %EXPECTED_TESTS% @echo "Fewer tests than expected." && exit /b 1
 
-@echo Done.
+@echo Done. %ACTUAL_TESTS% tests run.
+exit /b 0
+
+:RunTest
+  cd "%~dp1" || exit /b 1
+  "%~1" %2 %3 %4 %5 %6 %7 %8 %9 || exit /b 1
+  set /a ACTUAL_TESTS=%ACTUAL_TESTS%+1
+exit /b 0
