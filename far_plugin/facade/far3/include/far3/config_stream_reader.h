e4cda4943b8dd70a1f74a363b032a7641f1af891
EugeneManushkin <eugene.manushkin@gmail.com>
2025-09-19 12:13:35 +0300

    Implement ConfigDataWriter interface

--- far_plugin/facade/far3/include/far3/config_stream_reader.h
+++ far_plugin/facade/far3/include/far3/config_stream_reader.h
@@ -15,12 +15,12 @@ namespace Far3
   class ConfigStreamReader
   {
   public:
-    static std::unique_ptr<ConfigStreamReader> Create(
+    static std::unique_ptr<const ConfigStreamReader> Create(
       size_t maxKeyLen = 128
     , size_t maxValueLen = 65536
     , unsigned maxErrors = static_cast<int>(Plugin::ConfigFieldId::MaxFieldId)
     , unsigned maxLines = static_cast<int>(Plugin::ConfigFieldId::MaxFieldId) * 8
     );
-    virtual Plugin::Config Read(std::istream& stream, Plugin::ConfigDataMapper const& dataMapper) = 0;
+    virtual Plugin::Config Read(std::istream& stream, Plugin::ConfigDataMapper const& dataMapper) const = 0;
   };
 }
---
+++ tests/plugin/facade/far3/config_stream_writer/main.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include <far3/config_stream_writer.h>
+
+#include <plugin/config.h>
+#include <plugin/config_data_mapper.h>
+
+#include <sstream>
+#include <string>
+
+namespace Far3
+{
+  namespace Tests
+  {
+    TEST(ConfigStreamWriter, WritesConfig)
+    {
+      Plugin::Config const config;
+      std::stringstream stream;
+      auto const mapper = Plugin::ConfigDataMapper::Create();
+      ASSERT_NO_THROW(ConfigStreamWriter::Create()->Write(stream, *mapper, config));
+
+      for (int i = 0; i < static_cast<int>(Plugin::ConfigFieldId::MaxFieldId); ++i)
+      {
+        std::string line;
+        std::getline(stream, line);
+        auto field = mapper->Get(i, config);
+        ASSERT_EQ(line, field.key + "=" + field.value);
+      }
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
