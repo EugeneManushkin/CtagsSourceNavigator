f128330bdc8ad65e9ef628ba989003081b85ec3f
EugeneManushkin <eugene.manushkin@gmail.com>
2025-11-25 17:41:38 +0300

    Implement default button in ConfigValueDialog

--- far_plugin/facade/far3/src/config_value_dialog.cpp
+++ far_plugin/facade/far3/src/config_value_dialog.cpp
@@ -38,20 +38,152 @@ namespace
     return result;
   }
 
+  enum class DialogResult
+  {
+    Ok,
+    Cancel,
+    Default,
+  };
+
+  class Dialog
+  {
+  public:
+    Dialog(std::string const& value, ConfigFieldType type, int textId, PluginStartupInfo const& i, Guid const& pluginGuid);
+    DialogResult Show();
+    std::string GetValue() const;
+
+  private:
+    intptr_t Bottom() const;
+    void PushCheckbox(std::string const& value, int textId);
+    void PushSizebox(std::string const& value, int textId);
+    void PushEditbox(std::string const& value, int textId);
+    WideString GetText() const;
+    std::string GetCheckboxValue() const;
+    std::string GetSizeboxValue() const;
+    std::string GetEditboxValue() const;
+
+  private:
+    ConfigFieldType const Type;
+    PluginStartupInfo const& I;
+    Guid const& PluginGuid;
+    intptr_t CtrlId;
+    intptr_t CancelId;
+    intptr_t DefaultId;
+    WideString Value;
+    void* Handle;
+    std::vector<FarDialogItem> Items;
+  };
+
+  Dialog::Dialog(std::string const& value, ConfigFieldType type, int textId, PluginStartupInfo const& i, Guid const& pluginGuid)
+    : Type(type)
+    , I(i)
+    , PluginGuid(pluginGuid)
+  {
+    Items.push_back(MakeItem(DI_DOUBLEBOX, BoxPadding, Bottom(), Width - 1 - BoxPadding, 0));
+    if (type == ConfigFieldType::Flag) PushCheckbox(value, textId);
+    else if (type == ConfigFieldType::Size) PushSizebox(value, textId);
+    else PushEditbox(value, textId);
+    CtrlId = Items.size() - 1;
+    Items.push_back(MakeItem(DI_TEXT, InnerPadding, Bottom(), Width - 1 - InnerPadding, 0, nullptr, DIF_SEPARATOR|DIF_BOXCOLOR));
+    Items.push_back(MakeItem(DI_BUTTON, 0, Bottom(), 0, 0, I.GetMsg(&PluginGuid, MOk), DIF_CENTERGROUP));
+    Items.push_back(MakeItem(DI_BUTTON, 0, Bottom() - 1, 0, 0, I.GetMsg(&PluginGuid, MDefault), DIF_CENTERGROUP));
+    DefaultId = Items.size() - 1;
+    Items.push_back(MakeItem(DI_BUTTON, 0, Bottom() - 1, 0, 0, I.GetMsg(&PluginGuid, MCancel), DIF_CENTERGROUP));
+    CancelId = Items.size() - 1;
+    Items.front().Y2 = Bottom();
+    intptr_t const autocenterX = -1, autocenterY = -1;
+    Handle = I.DialogInit(
+      &PluginGuid,
+      &DialogGuid,
+      autocenterX,
+      autocenterY,
+      Width,
+      Bottom() + 2,
+      nullptr,
+      Items.data(),
+      Items.size(),
+      0,
+      FDLG_NONE,
+      nullptr,
+      nullptr
+    );
+
+    if (Handle == INVALID_HANDLE_VALUE)
+      throw std::runtime_error("DialogInit: failed to create config value dialog");
+  }
+
+  DialogResult Dialog::Show()
+  {
+    auto const selected = I.DialogRun(Handle);
+    return selected == -1 || selected == CancelId ? DialogResult::Cancel
+         : selected == DefaultId ? DialogResult::Default
+         : DialogResult::Ok;
+  }
+
+  std::string Dialog::GetValue() const
+  {
+    return Type == ConfigFieldType::Flag ? GetCheckboxValue()
+         : Type == ConfigFieldType::Size ? GetSizeboxValue()
+         : GetEditboxValue();
+  }
+
+  intptr_t Dialog::Bottom() const
+  {
+    return !Items.empty() ? Items.back().Y1 + 1 : 1;
+  }
+
+  void Dialog::PushCheckbox(std::string const& value, int textId)
+  {
+    Items.push_back(MakeItem(DI_CHECKBOX, InnerPadding, Bottom(), 0, 0, I.GetMsg(&PluginGuid, textId), DIF_FOCUS));
+    Items.back().Selected = value == "true" ? 1 : 0;
+  }
+
+  void Dialog::PushSizebox(std::string const& value, int textId)
+  {
+    Value = ToString(value);
+    Items.push_back(MakeItem(DI_TEXT, InnerPadding, Bottom(), 0, 0, I.GetMsg(&PluginGuid, textId)));
+    Items.push_back(MakeItem(DI_FIXEDIT, InnerPadding, Bottom(), Width - 1 - InnerPadding, 0, Value.c_str(), DIF_MASKEDIT|DIF_FOCUS));
+    Items.back().Mask = SizeboxMask;
+  }
+
+  void Dialog::PushEditbox(std::string const& value, int textId)
+  {
+    Value = ToString(value);
+    Items.push_back(MakeItem(DI_TEXT, InnerPadding, Bottom(), 0, 0, I.GetMsg(&PluginGuid, textId)));
+    Items.push_back(MakeItem(DI_EDIT, InnerPadding, Bottom(), Width - 1 - InnerPadding, 0, Value.c_str(), DIF_FOCUS));
+  }
+
+  WideString Dialog::GetText() const
+  {
+    FarDialogItemData item = {sizeof(FarDialogItemData)};
+    item.PtrLength = I.SendDlgMessage(Handle, DM_GETTEXT, CtrlId, 0);
+    std::vector<WideString::value_type> buf(item.PtrLength + 1);
+    item.PtrData = buf.data();
+    I.SendDlgMessage(Handle, DM_GETTEXT, CtrlId, &item);
+    return WideString(item.PtrData, item.PtrLength);
+  }
+
+  std::string Dialog::GetCheckboxValue() const
+  {
+    return I.SendDlgMessage(Handle, DM_GETCHECK, CtrlId, 0) == BSTATE_CHECKED ? "true" : "false";
+  }
+
+  std::string Dialog::GetSizeboxValue() const
+  {
+    auto const text = GetText();
+    return ToStdString(text.substr(0, text.find_first_not_of(L"0123456789")));
+  }
+
+  std::string Dialog::GetEditboxValue() const
+  {
+    return ToStdString(GetText());
+  }
+
   class ConfigValueDialogImpl : public Far3::ConfigValueDialog
   {
   public:
     ConfigValueDialogImpl(PluginStartupInfo const& i, Guid const& pluginGuid);
-    std::pair<bool, std::string> Show(std::string const& initValue, ConfigFieldType type, int textId) const override;
-
-  private:
-    intptr_t PushCheckbox(std::string const& value, int textId, intptr_t& y, std::vector<FarDialogItem>& items) const;
-    intptr_t PushSizebox(WideString::value_type const* value, int textId, intptr_t& y, std::vector<FarDialogItem>& items) const;
-    intptr_t PushEditbox(WideString::value_type const* value, int textId, intptr_t& y, std::vector<FarDialogItem>& items) const;
-    WideString GetText(intptr_t ctrlId, void* handle) const;
-    std::string GetCheckboxValue(intptr_t ctrlId, void* handle) const;
-    std::string GetSizeboxValue(intptr_t ctrlId, void* handle) const;
-    std::string GetEditboxValue(intptr_t ctrlId, void* handle) const;
+    std::pair<bool, std::string> Show(std::string const& initValue, std::string const& defaultValue, ConfigFieldType type, int textId) const override;
 
   private:
     PluginStartupInfo const I;
@@ -64,97 +196,18 @@ namespace
   {
   }
 
-  std::pair<bool, std::string> ConfigValueDialogImpl::Show(std::string const& initValue, ConfigFieldType type, int textId) const
+  std::pair<bool, std::string> ConfigValueDialogImpl::Show(std::string const& initValue, std::string const& defaultValue, ConfigFieldType type, int textId) const
   {
-    intptr_t y = 0;
-    std::vector<FarDialogItem> items;
-    items.push_back(MakeItem(DI_DOUBLEBOX, BoxPadding, ++y, Width - 1 - BoxPadding, 0));
-    auto init = type != ConfigFieldType::Flag ? ToString(initValue) : WideString();
-    intptr_t ctrlId = type == ConfigFieldType::Flag ? PushCheckbox(initValue, textId, y, items)
-                    : type == ConfigFieldType::Size ? PushSizebox(init.c_str(), textId, y, items)
-                    : PushEditbox(init.c_str(), textId, y, items);
-    items.push_back(MakeItem(DI_TEXT, InnerPadding, ++y, Width - 1 - InnerPadding, 0, nullptr, DIF_SEPARATOR|DIF_BOXCOLOR));
-    items.push_back(MakeItem(DI_BUTTON, 0, ++y, 0, 0, I.GetMsg(&PluginGuid, MOk), DIF_CENTERGROUP));
-    items.push_back(MakeItem(DI_BUTTON, 0, y, 0, 0, I.GetMsg(&PluginGuid, MCancel), DIF_CENTERGROUP));
-    intptr_t const cancelIndex = items.size() - 1;
-    items.front().Y2 = ++y;
-
-    intptr_t const autocenterX = -1, autocenterY = -1;
-    auto handle = I.DialogInit(
-      &PluginGuid,
-      &DialogGuid,
-      autocenterX,
-      autocenterY,
-      Width,
-      y + 2,
-      nullptr,
-      items.data(),
-      items.size(),
-      0,
-      FDLG_NONE,
-      nullptr,
-      nullptr
-    );
-
-    if (handle == INVALID_HANDLE_VALUE)
-      throw std::runtime_error("DialogInit: failed to create config value dialog");
-
-    auto const selected = I.DialogRun(handle);
-    if (selected == -1 || selected == cancelIndex)
-      return std::make_pair(false, "");
-
-    std::string result = type == ConfigFieldType::Flag ? GetCheckboxValue(ctrlId, handle)
-                       : type == ConfigFieldType::Size ? GetSizeboxValue(ctrlId, handle)
-                       : GetEditboxValue(ctrlId, handle);
-    return std::make_pair(true, std::move(result));
-  }
-
-  intptr_t ConfigValueDialogImpl::PushCheckbox(std::string const& value, int textId, intptr_t& y, std::vector<FarDialogItem>& items) const
-  {
-    items.push_back(MakeItem(DI_CHECKBOX, InnerPadding, ++y, 0, 0, I.GetMsg(&PluginGuid, textId), DIF_FOCUS));
-    items.back().Selected = value == "true" ? 1 : 0;
-    return items.size() - 1;
-  }
-
-  intptr_t ConfigValueDialogImpl::PushSizebox(WideString::value_type const* value, int textId, intptr_t& y, std::vector<FarDialogItem>& items) const
-  {
-    items.push_back(MakeItem(DI_TEXT, InnerPadding, ++y, 0, 0, I.GetMsg(&PluginGuid, textId)));
-    items.push_back(MakeItem(DI_FIXEDIT, InnerPadding, ++y, Width - 1 - InnerPadding, 0, value, DIF_MASKEDIT|DIF_FOCUS));
-    items.back().Mask = SizeboxMask;
-    return items.size() - 1;
-  }
-
-  intptr_t ConfigValueDialogImpl::PushEditbox(WideString::value_type const* value, int textId, intptr_t& y, std::vector<FarDialogItem>& items) const
-  {
-    items.push_back(MakeItem(DI_TEXT, InnerPadding, ++y, 0, 0, I.GetMsg(&PluginGuid, textId)));
-    items.push_back(MakeItem(DI_EDIT, InnerPadding, ++y, Width - 1 - InnerPadding, 0, value, DIF_FOCUS));
-    return items.size() - 1;
-  }
-
-  WideString ConfigValueDialogImpl::GetText(intptr_t ctrlId, void* handle) const
-  {
-    FarDialogItemData item = {sizeof(FarDialogItemData)};
-    item.PtrLength = I.SendDlgMessage(handle, DM_GETTEXT, ctrlId, 0);
-    std::vector<WideString::value_type> buf(item.PtrLength + 1);
-    item.PtrData = buf.data();
-    I.SendDlgMessage(handle, DM_GETTEXT, ctrlId, &item);
-    return WideString(item.PtrData, item.PtrLength);
-  }
-
-  std::string ConfigValueDialogImpl::GetCheckboxValue(intptr_t ctrlId, void* handle) const
-  {
-    return I.SendDlgMessage(handle, DM_GETCHECK, ctrlId, 0) == BSTATE_CHECKED ? "true" : "false";
-  }
-
-  std::string ConfigValueDialogImpl::GetSizeboxValue(intptr_t ctrlId, void* handle) const
-  {
-    auto text = GetText(ctrlId, handle);
-    return ToStdString(text.substr(0, text.find_first_not_of(L"0123456789")));
-  }
-
-  std::string ConfigValueDialogImpl::GetEditboxValue(intptr_t ctrlId, void* handle) const
-  {
-    return ToStdString(GetText(ctrlId, handle));
+    auto result = DialogResult::Ok;
+    do
+    {
+      Dialog dialog(result == DialogResult::Default ? defaultValue : initValue, type, textId, I, PluginGuid);
+      result = dialog.Show();
+      if (result == DialogResult::Ok)
+        return std::make_pair(true, dialog.GetValue());
+    }
+    while(result == DialogResult::Default);
+    return std::make_pair(false, "");
   }
 }
 
